b721bd11e71e4edc602a37defb8aa4161fdfa257
Richards0n <richardson.douglas.31@gmail.com>
2024-06-30 12:14:26 -0300

    feat: Add getMembership route to API server

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,45 @@
+import { auth } from '@/http/middlewares/auth'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function getMembership(app: FastifyInstance) {
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.get(
+			'/orgs/:slug/membership',
+			{
+				schema: {
+					tags: ['Organizations'],
+					summary: 'Get membership on organization',
+					security: [{ bearerAuth: [] }],
+					params: z.object({
+						slug: z.string()
+					}),
+					response: {
+						200: z.object({
+							membership: z.object({
+								id: z.string(),
+								role: roleSchema,
+								organizationId: z.string().uuid()
+							})
+						})
+					}
+				}
+			},
+			async (req, res) => {
+				const { slug } = req.params
+				const { membership } = await req.getUserMemberShop(slug)
+
+				return res.send({
+					membership: {
+						id: membership.id,
+						role: membership.role,
+						organizationId: membership.organizationId
+					}
+				})
+			}
+		)
+}
