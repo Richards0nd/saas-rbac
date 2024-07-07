d3dc85e6643b7a743754968fdfb6dbc6adf8ae34
Richards0n <richardson.douglas.31@gmail.com>
2024-07-07 00:03:47 -0300

    feat: Add transferOrganization route to API server

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { getOrganization } from './routes/orgs/get-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -71,6 +72,7 @@ app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
 
 app.listen({ host: env.SERVER_HOST, port: env.SERVER_PORT }).then(() => {
 	console.log(
